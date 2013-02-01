c00a4652cf049d1148990dc2d3b80983acc5533e
Carlos <Carlos@Angelo>
2013-02-01 16:23:02 -0300

    atualizando a interface gráfica

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ import modelo.Resposta;
 import negocio.Grupo_I;
 import negocio.Grupo_II;
 import util.JPAUtil;
+import util.TelaGrupoI;
 import view.ViewGrupos;
 
 public class Main {
@@ -16,6 +17,24 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
+        
+        
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(TelaGrupoI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(TelaGrupoI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(TelaGrupoI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(TelaGrupoI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
 
       
          ViewGrupos view = new ViewGrupos();
