1752f5d3f843cc257a0118eba3465f6f9bdd66f2
alisson <alisson@Alisson.alisson>
2013-03-08 13:22:40 -0300

    Alteração feita na classe ViewExameClinico.

--- src/view/ViewExameClinico.java
+++ src/view/ViewExameClinico.java
@@ -1720,7 +1720,7 @@ public class ViewExameClinico extends JFrame {
                 exame.setRq4CMm(rqE4c);
                 exame.setRq4DMm(rqE4d);
                 
-                //Modificar o código aqui
+                //Modificar o código aqui, está faltando um atributo
                 exame.setRq5ADir(ComboBoxQE5ADireito.getSelectedItem().toString());
                 exame.setRq5AEsq(ComboBoxQE5AEsquerdo.getSelectedItem().toString());
                 exame.setRq5AMedEstMm(campoQE5ADireito.getText());
